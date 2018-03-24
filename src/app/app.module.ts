b1e93c749dd328889e9fca668e524a8d62069489
Alex Shovkoplyas <alexshovkoplyas@gmail.com>
2018-03-24 16:56:33 +0200

    add: lazy loading, test module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from "@angular/router";
 
-
+import { TestModule } from "./tests/test.module";
 import { AppComponent } from './app.component';
 
+const routes : Routes = [
+  {path:'test', loadChildren : './tests/test.module#TestModule'},
+  {path:'**', redirectTo: 'test'}
+]
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forChild(routes),
+    TestModule
   ],
   providers: [],
   bootstrap: [AppComponent]
