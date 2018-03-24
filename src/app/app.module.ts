18253c3830464429d57477fdfc610b03e98decb1
Alex Shovkoplyas <alexshovkoplyas@gmail.com>
2018-03-24 17:42:53 +0200

    fuck

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-<<<<<<< HEAD
+
 import { Routes, RouterModule } from "@angular/router";
 
 // import { TestModule } from "./tests/test.module";
-=======
-import { RouterModule, Routes } from "@angular/router";
 
-import { TestModule } from "./tests/test.module";
->>>>>>> b1e93c749dd328889e9fca668e524a8d62069489
+
 import { AppComponent } from './app.component';
 
 const routes : Routes = [
@@ -22,14 +19,9 @@ const routes : Routes = [
   ],
   imports: [
     BrowserModule,
-<<<<<<< HEAD
-    // TestModule,
-    RouterModule.forRoot(routes),    
-=======
-    RouterModule.forChild(routes),
-    TestModule
->>>>>>> b1e93c749dd328889e9fca668e524a8d62069489
+    RouterModule.forChild(routes)
   ],
+
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/tests/test.module.ts
+++ src/app/tests/test.module.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
@@ -17,17 +16,6 @@ const routes: Routes = [
     ]
   },
   { path: '**', redirectTo: 'cards' }
-=======
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { RouterModule, Routes } from "@angular/router";
-
-import { CardsComponent } from './components/cards/cards.component';
-
-const routes : Routes = [
-  {path:'cards', component: CardsComponent},
-  {path:'**', redirectTo:'cards'}
->>>>>>> b1e93c749dd328889e9fca668e524a8d62069489
 ]
 
 @NgModule({
@@ -35,13 +23,10 @@ const routes : Routes = [
     CommonModule,
     RouterModule.forChild(routes)
   ],
-<<<<<<< HEAD
+
   declarations: [
     TestAppComponent,
     CardsComponent
   ]
-=======
-  declarations: [CardsComponent]
->>>>>>> b1e93c749dd328889e9fca668e524a8d62069489
 })
 export class TestModule { }
