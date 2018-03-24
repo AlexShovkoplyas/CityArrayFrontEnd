6f1e518e234b17675daa22fb710d1e4336851440
Alex Shovkoplyas <alexshovkoplyas@gmail.com>
2018-03-24 17:50:44 +0200

    final change 3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from "@angular/router";
-
-// import { TestModule } from "./tests/test.module";
-
 
 import { AppComponent } from './app.component';
 
